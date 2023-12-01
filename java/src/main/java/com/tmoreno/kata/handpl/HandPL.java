ea8c90c63d00e2b82186c66dd243413c648b2fc8
tmoreno <tmorenobernal@gmail.com>
2023-12-01 20:26:37 +0100

    Add Memory collaborator

--- java/src/main/java/com/tmoreno/kata/handpl/HandPL.java
+++ java/src/main/java/com/tmoreno/kata/handpl/HandPL.java
@@ -2,13 +2,20 @@ package com.tmoreno.kata.handpl;
 
 public final class HandPL {
 
+  private final Memory memory;
   private final Printer printer;
 
-  public HandPL(Printer printer) {
+  public HandPL(Memory memory, Printer printer) {
     this.printer = printer;
+    this.memory = memory;
   }
 
   public void execute(String instructions) {
-
+    for (String instruction : instructions.split("")) {
+      switch (instruction) {
+        case "👆" -> memory.increment();
+        case "👊" -> printer.print(memory.currentValuePosition());
+      }
+    }
   }
 }
---
+++ java/src/main/java/com/tmoreno/kata/handpl/Memory.java
@@ -0,0 +1,11 @@
+package com.tmoreno.kata.handpl;
+
+public final class Memory {
+  public void increment() {
+    throw new UnsupportedOperationException();
+  }
+
+  public char currentValuePosition() {
+    throw new UnsupportedOperationException();
+  }
+}
