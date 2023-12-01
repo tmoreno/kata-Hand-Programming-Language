acbcea1dbb3a4dfba50f8cdb0c06bc9e0509216e
tmoreno <tmorenobernal@gmail.com>
2023-12-01 21:55:52 +0100

    Refactor. Use a parametrized test

--- java/src/test/java/com/tmoreno/kata/handpl/MemoryTest.java
+++ java/src/test/java/com/tmoreno/kata/handpl/MemoryTest.java
@@ -1,57 +1,38 @@
 package com.tmoreno.kata.handpl;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemoryTest {
 
-  @Test
-  void should_be_zero_when_memory_is_initialized() {
-    Memory memory = new Memory();
-
-    assertThat(memory.currentValuePosition()).isEqualTo((char) 0);
+  private static Stream<Arguments> argumentsForIncrements() {
+    return Stream.of(
+      Arguments.of(0, 0),
+      Arguments.of(1, 1),
+      Arguments.of(2, 2),
+      Arguments.of(254, 254),
+      Arguments.of(255, 255),
+      Arguments.of(256, 0),
+      Arguments.of(257, 1)
+    );
   }
 
-  @Test
-  void should_increment_current_value_by_one() {
+  @MethodSource("argumentsForIncrements")
+  @ParameterizedTest
+  void should_increment_as_expected(int times, int expectedValue) {
     Memory memory = new Memory();
 
-    memory.increment();
-
-    assertThat(memory.currentValuePosition()).isEqualTo((char) 1);
-  }
-
-  @Test
-  void should_increment_current_value_by_two() {
-    Memory memory = new Memory();
-
-    memory.increment();
-    memory.increment();
-
-    assertThat(memory.currentValuePosition()).isEqualTo((char) 2);
-  }
-
-  @Test
-  void should_be_255_when_increment_255_times() {
-    Memory memory = new Memory();
-
-    for (int i = 0; i < 255; i++) {
+    for (int i = 0; i < times; i++) {
       memory.increment();
     }
 
-    assertThat(memory.currentValuePosition()).isEqualTo((char) 255);
-  }
-
-  @Test
-  void should_be_zero_when_increment_256_times() {
-    Memory memory = new Memory();
-
-    for (int i = 0; i < 256; i++) {
-      memory.increment();
-    }
-
-    assertThat(memory.currentValuePosition()).isEqualTo((char) 0);
+    assertThat(memory.currentValuePosition()).isEqualTo((char) expectedValue);
   }
 
   @Test
