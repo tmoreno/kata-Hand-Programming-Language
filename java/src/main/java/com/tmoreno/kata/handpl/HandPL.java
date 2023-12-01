312a3211952b80b4342c31741bc815d585613872
tmoreno <tmorenobernal@gmail.com>
2023-12-01 20:46:27 +0100

    Split each instruction and execute action

--- java/src/main/java/com/tmoreno/kata/handpl/HandPL.java
+++ java/src/main/java/com/tmoreno/kata/handpl/HandPL.java
@@ -6,16 +6,19 @@ public final class HandPL {
   private final Printer printer;
 
   public HandPL(Memory memory, Printer printer) {
-    this.printer = printer;
     this.memory = memory;
+    this.printer = printer;
   }
 
   public void execute(String instructions) {
-    for (String instruction : instructions.split("")) {
-      switch (instruction) {
-        case "👆" -> memory.increment();
-        case "👊" -> printer.print(memory.currentValuePosition());
-      }
-    }
+    instructions
+      .codePoints()
+      .mapToObj(Character::toString)
+      .forEach(instruction -> {
+        switch (instruction) {
+          case "👆" -> memory.increment();
+          case "👊" -> printer.print(memory.currentValuePosition());
+        }
+      });
   }
 }
